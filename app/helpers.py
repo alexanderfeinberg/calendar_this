ffa712c56685d766f7e9257277cff3bd7a11bd67
alexander1fein <feinberg.alexander@gmail.com>
2022-11-01 16:27:40 -0400

    Validations

--- app/helpers.py
+++ app/helpers.py
@@ -1,4 +1,23 @@
 from datetime import datetime
+import sqlite3
+
+
+def handle_submit(form, DB_FILE):
+    params = {
+        'name': form.name.data,
+        'start_datetime': datetime.combine(form.start_date.data, form.start_time.data),
+        'end_datetime': datetime.combine(form.end_date.data, form.end_time.data),
+        'description': form.description.data,
+        'private': form.private.data
+    }
+
+    with sqlite3.connect(DB_FILE) as conn:
+        curs = conn.cursor()
+        curs.execute("""
+        INSERT INTO appointments (name, start_datetime, end_datetime, description, private)
+        VALUES (:name, :start_datetime, :end_datetime, :description, :private)
+
+        """, {'name': params['name'], 'start_datetime': params['start_datetime'], 'end_datetime': params['end_datetime'], 'description': params['description'], 'private': params['private']})
 
 
 def convert_to_date(date):
@@ -14,3 +33,12 @@ def format_appointments(appointments):
         appt_list.append(
             {'id': appt[0], 'name': appt[1], 'start_datetime': start_date, 'end_datetime': end_date, 'start_date_abv': start_date.strftime('%H:%M'), 'end_date_abv': end_date.strftime('%H:%M')})
     return appt_list
+
+
+def load_data(DB_FILE):
+    with sqlite3.connect(DB_FILE) as conn:
+        curs = conn.cursor()
+        curs.execute(
+            'SELECT id, name, start_datetime, end_datetime FROM appointments ORDER BY start_datetime')
+        appointments = curs.fetchall()
+    return appointments
--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,25 @@
+from crypt import methods
 from flask import Blueprint, render_template, redirect
 import os
 import sqlite3
 from .helpers import *
+from .forms import AppointmentForm
+
+
 bp = Blueprint("main", __name__, '/')
 DB_FILE = os.environ.get("DB_FILE")
 
 
-@bp.route('/')
+@bp.route('/', methods=["GET", "POST"])
 def main():
-    with sqlite3.connect(DB_FILE) as conn:
-        curs = conn.cursor()
-        curs.execute(
-            'SELECT id, name, start_datetime, end_datetime FROM appointments ORDER BY start_datetime')
-        appointments = curs.fetchall()
+    form = AppointmentForm()
 
+    if form.validate_on_submit():
+        print("Handling submit")
+        handle_submit(form, DB_FILE)
+
+    appointments = load_data(DB_FILE)
     appt_list = format_appointments(appointments)
     print(appt_list)
 
-    return render_template('main.html', rows=appt_list)
+    return render_template('main.html', rows=appt_list, form=form)
